853dcfb0b9d9e56b31482aaff051fafcd5f76fef
Jesse Shawl <jesse@jshawl.com>
2014-10-25 10:40:08 -0400

    add some links

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,25 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :update, :delete]
   def index
     
+  end
+
+  def show
+
+  end
+  def create
+    @project = Project.new(project_params.merge(user:current_user))
+    if @project.save
+      redirect_to root_path 
+    end
+  end
+
+
+  private
+  def set_project
+    @project = Project.find( params[:id] )
+  end
+  def project_params
+    params.require(:project).permit(:title, :description)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
   def index
+    @user = current_user || User.new
+    @projects = Project.all
+    @project = Project.new
   end
 end
